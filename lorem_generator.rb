a0b8daf1fd4d452ffa14a43771cfcbbabf7149d7
Rubén Ignacio Castillo Díaz <ruben.cas@outlook.com>
2020-08-28 12:17:48 -0400

    add lorem_generator.rb

---
+++ lorem_generator.rb
@@ -0,0 +1,4 @@
+n= ARGV[0].to_i
+n.times do |i|
+    puts "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis luctus tortor quam, vitae tristique nisi suscipit quis. Duis pretium aliquam leo, quis dapibus elit tempus sit amet. Donec et elit eget leo lobortis tempor. Maecenas augue ligula, pretium in ex cursus, rhoncus iaculis lorem. Phasellus pulvinar risus sapien, vitae imperdiet lectus ultrices eget. Fusce vestibulum tellus vitae vulputate viverra. Vivamus nec risus porttitor eros viverra pellentesque quis vitae lorem. Aliquam erat volutpat.\n \n"
+end
